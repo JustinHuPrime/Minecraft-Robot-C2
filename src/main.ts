482f043cf8c3c13230bd0f6a163a8e0a2537f46c
Justin Hu <justin.hu@alumni.ubc.ca>
2021-03-01 21:26:18 -0800

    select and exec commands

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import * as WebSocket from "ws";
 import * as Readline from "readline";
 
+enum TurtleStatus {
+  IDLE,
+  BUSY,
+}
+
 class Turtle {
-  private name: string;
-  private ws: WebSocket;
+  public name: string;
+  public ws: WebSocket;
+  public status: TurtleStatus;
 
   constructor(name: string, ws: WebSocket) {
     this.name = name;
     this.ws = ws;
+    this.status = TurtleStatus.IDLE;
   }
 };
 
@@ -15,19 +22,63 @@ let turtles: Turtle[] = [];
 let active: Turtle | null = null;
 
 const wss = new WebSocket.Server({ port: 8888 });
+const io = Readline.createInterface({ input: process.stdin, output: process.stdout });
 
 wss.on("connection", (ws) => {
   ws.once("message", (name: string) => {
     turtles.push(new Turtle(name, ws));
+    io.write(`Turtle ${name} connected`);
   });
 });
 
-const io = Readline.createInterface({ input: process.stdin, output: process.stdout });
+async function commandLoop(): Promise<void> {
+  while (true) {
+    const line: string = await new Promise<string>((resolve, _) => { io.question(active !== null ? `${active.name}> ` : "> ", resolve); });
 
-function commandline() {
-  io.question("> ", (answer: string) => {
-    io.write(`${answer}\n`);
-    setTimeout(commandline, 0);
-  });
+    if (line.trim() === "") continue;
+
+    const tokens = line.split(/\s+/);
+    switch (tokens[0]) {
+      case "select": {
+        if (tokens.length !== 2) {
+          io.write("select expects one argument - the name of the turtle to select");
+          continue;
+        }
+
+        const selected: Turtle | undefined = turtles.find((turtle) => turtle.name === tokens[1]);
+        if (selected === undefined) {
+          io.write(`no such turtle ${tokens[1]}`);
+          continue;
+        }
+
+        active = selected;
+        break;
+      }
+      case "exec": {
+        if (tokens.length < 2) {
+          io.write("exec expects a string - the code to execute");
+          continue;
+        }
+        if (active === null) {
+          io.write("exec requires an active turtle");
+          continue;
+        }
+
+        const matches = line.match(/exec\s+(.+)/);
+        const message = (matches as RegExpMatchArray)[1];
+
+        active.ws.send(message);
+        io.write(await new Promise<string>((resolve, reject) => {
+          (active as Turtle).ws.once("error", reject);
+          (active as Turtle).ws.once("message", resolve);
+        }));
+        break;
+      }
+      default: {
+        io.write(`no such command ${tokens[0]}`);
+        continue;
+      }
+    }
+  }
 }
-setTimeout(commandline, 0);
+commandLoop();
