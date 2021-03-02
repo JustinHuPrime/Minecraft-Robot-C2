a6dac47335cf7a2c1a9af58643e86ae672873f9b
Justin Hu <justin.hu@alumni.ubc.ca>
2021-03-01 21:59:22 -0800

    inventory and better error handling

--- src/main.ts
+++ src/main.ts
@@ -28,57 +28,94 @@ wss.on("connection", (ws) => {
   ws.once("message", (name: string) => {
     turtles.push(new Turtle(name, ws));
     io.write(`\nTurtle ${name} connected\n`);
+    ws.once("close", () => {
+      turtles.filter((turtle) => turtle.name !== name);
+      if (active !== null && active.name === name)
+        active = null;
+      io.write(`\nTurtle ${name} disconnected\n`);
+    });
   });
 });
 
+async function getReply(): Promise<string> {
+  if (active === null)
+    return Promise.reject(new Error("no selected turtle"));
+
+  return new Promise<string>((resolve, reject) => {
+    (active as Turtle).ws.once("error", reject);
+    (active as Turtle).ws.once("message", resolve);
+  });
+}
+
 async function commandLoop(): Promise<void> {
   while (true) {
-    const line: string = await new Promise<string>((resolve, _) => { io.question(active !== null ? `${active.name}> ` : "> ", resolve); });
+    try {
+      const line: string = await new Promise<string>((resolve, _) => { io.question(active !== null ? `${active.name}> ` : "> ", resolve); });
 
-    if (line.trim() === "") continue;
+      if (line.trim() === "") continue;
 
-    const tokens = line.split(/\s+/);
-    switch (tokens[0]) {
-      case "select": {
-        if (tokens.length !== 2) {
-          io.write("select expects one argument - the name of the turtle to select\n");
+      const tokens = line.split(/\s+/);
+      switch (tokens[0]) {
+        case "select": {
+          if (tokens.length !== 2) {
+            io.write("select expects one argument - the name of the turtle to select\n");
+            continue;
+          }
+
+          const selected = turtles.find((turtle) => turtle.name === tokens[1]);
+          if (selected === undefined) {
+            io.write(`no such turtle ${tokens[1]}\n`);
+            continue;
+          }
+
+          active = selected;
+          break;
+        }
+        case "inventory": {
+          if (tokens.length !== 1) {
+            io.write("inventory expects no arguments\n");
+            continue;
+          }
+          if (active === null) {
+            io.write("inventory requires an active turtle\n");
+            continue;
+          }
+
+          active.ws.send("return turtle.getSelectedSlot()");
+          const selected = Number.parseInt(await getReply());
+          for (let idx = 1; idx <= 16; ++idx) {
+            active.ws.send(`return turtle.getItemDetail(${idx})`);
+            if (selected === idx)
+              io.write(`* ${await getReply()}`);
+            else
+              io.write(`  ${await getReply()}`);
+          }
+          break;
+        }
+        case "exec": {
+          if (tokens.length < 2) {
+            io.write("exec expects a string - the code to execute\n");
+            continue;
+          }
+          if (active === null) {
+            io.write("exec requires an active turtle\n");
+            continue;
+          }
+
+          const matches = line.match(/exec\s+(.+)/);
+          const message = (matches as RegExpMatchArray)[1];
+
+          active.ws.send(message);
+          io.write(`${await getReply()}\n`);
+          break;
+        }
+        default: {
+          io.write(`no such command ${tokens[0]}\n`);
           continue;
         }
-
-        const selected: Turtle | undefined = turtles.find((turtle) => turtle.name === tokens[1]);
-        if (selected === undefined) {
-          io.write(`no such turtle ${tokens[1]}\n`);
-          continue;
-        }
-
-        active = selected;
-        break;
       }
-      case "exec": {
-        if (tokens.length < 2) {
-          io.write("exec expects a string - the code to execute\n");
-          continue;
-        }
-        if (active === null) {
-          io.write("exec requires an active turtle\n");
-          continue;
-        }
-
-        const matches = line.match(/exec\s+(.+)/);
-        const message = (matches as RegExpMatchArray)[1];
-
-        active.ws.send(message);
-        const response = await new Promise<string>((resolve, reject) => {
-          (active as Turtle).ws.once("error", reject);
-          (active as Turtle).ws.once("message", resolve);
-        })
-        io.write(`${response}\n`);
-        break;
-      }
-      default: {
-        io.write(`no such command ${tokens[0]}\n`);
-        continue;
-      }
+    } catch (e) {
+      io.write(`Error: ${e}\n`);
     }
   }
 }
